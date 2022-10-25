511e443c35a013fcf0ab43c5e51bb14ce944bf71
Ömer Faruk Dağ <omerrfarukkdagg@gmail.com>
2022-10-25 13:38:01 +0300

    Update

--- api.php
+++ api.php
@@ -1,29 +1,24 @@
 <?php
-require_once 'vendor/autoload.php';
-$client = new GuzzleHttp\Client();
-
-$translate = function (string $text, string $to = 'tr', string $from = 'auto') use ($client): string {
+$translate = function (string $text, string $to = 'tr', string $from = 'auto'): string {
     $text = urlencode($text);
     $from = strtolower($from);
     $to = strtolower($to);
 
-    $response = $client->request('GET', 'https://translate.googleapis.com/translate_a/single?', [
-        'query' => 'client=dict-chrome-ex&sl=' . $from . '&tl=' . $to . '&dt=t&q=' . $text,
-        'headers' => [
-            'User-Agent' => $_SERVER['HTTP_USER_AGENT'],
-        ]
-    ]);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://translate.googleapis.com/translate_a/single?client=dict-chrome-ex&sl=' . $from . '&tl=' . $to . '&dt=t&q=' . $text);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-    if ($response->getStatusCode() === 200 && $response->getReasonPhrase() === 'OK') {
-        $result = json_decode($response->getBody(), true);
-        $res = array();
-        if (isset($result) && !empty($result)) {
-            for ($i = 0; $i < count($result[0]); $i++) {
-                array_push($res, $result[0][$i][0]);
-            }
-            return htmlspecialchars(implode('', $res));
+    $result = json_decode($response, true);
+    if (isset($result) && !empty($result)) {
+        for ($i = 0; $i < count($result[0]); $i++) {
+            $data[] = $result[0][$i][0];
         }
+        return htmlspecialchars(implode('', $data));
     }
 };
 
-echo $translate(htmlspecialchars($_POST['text']), $_POST['language']);
+echo json_encode(['text' => $translate(htmlspecialchars($_POST['text']), $_POST['language'])]);
+?>
